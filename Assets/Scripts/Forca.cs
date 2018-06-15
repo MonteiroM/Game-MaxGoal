19cbe48f50b6fa95704522306585f8d09c01cdd8
Matheus Monteiro <matheusmonteirox@gmail.com>
2018-06-15 14:16:29 -0300

    First Commit

---
+++ Assets/Scripts/Forca.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Forca : MonoBehaviour {
+
+	private Rigidbody2D bola;
+	private float force = 0f;
+	private Rotacao rotacao;
+	[SerializeField]private Image seta2Img;
+
+
+	void Start () {
+		bola = GetComponent<Rigidbody2D> ();
+		rotacao = GetComponent<Rotacao> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		AplicaForca ();
+		ControlaForca ();
+	}
+
+	void AplicaForca(){
+		float x = force * Mathf.Cos (rotacao.zRotate * Mathf.Deg2Rad);
+		float y = force * Mathf.Sin (rotacao.zRotate * Mathf.Deg2Rad);
+
+
+		if(rotacao.liberaTiro){
+			bola.AddForce (new Vector2 (x, y));
+			rotacao.liberaTiro = false;
+		}
+	}
+
+	void ControlaForca(){
+		if (rotacao.liberaRotate) {
+			float moveX = Input.GetAxis ("Mouse X");
+
+			if (moveX < 0) {
+				seta2Img.fillAmount += 1 * Time.deltaTime;
+				force = seta2Img.fillAmount * 1000;
+			}
+
+			if (moveX > 0) {
+				seta2Img.fillAmount -= 1 * Time.deltaTime;
+				force = seta2Img.fillAmount * 1000;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Rotacao.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Rotacao : MonoBehaviour {
+
+	//posição seta
+	[SerializeField]private Transform posStart;
+	//seta
+	[SerializeField]private Image setaImg;
+	//angulo
+	public float zRotate;
+	public bool liberaRotate = false;
+	public bool liberaTiro = false;
+
+	void Start () {
+		PosicionaSeta ();
+		PosicionaBola ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RotacaoSeta ();
+		LimitaRotacao ();
+		InputDeRotacao ();
+	}
+
+	void PosicionaSeta(){
+		setaImg.rectTransform.position = posStart.position;
+	}
+
+	void PosicionaBola(){
+		this.gameObject.transform.position = posStart.position;
+	}
+
+	void RotacaoSeta(){
+		setaImg.rectTransform.eulerAngles = new Vector3 (0, 0, zRotate);
+	}
+
+	void InputDeRotacao(){
+
+		if(liberaRotate){
+			
+			float moveY = Input.GetAxis ("Mouse Y");
+
+			if (zRotate < 90) {
+				if (moveY > 0) {
+					zRotate += 2.5f;
+				}
+			}
+
+			if (zRotate > 0) {
+				if (moveY < 0) {
+					zRotate -= 2.5f;
+				}
+			}
+		} 
+	}
+
+	void LimitaRotacao(){
+		if (zRotate >= 90) {
+			zRotate = 90;
+		}
+
+		if (zRotate <= 0) {
+			zRotate = 0;
+		}
+	}
+
+	void OnMouseDown(){
+		liberaRotate = true;
+	}
+
+	void OnMouseUp(){
+		liberaRotate = false;
+		liberaTiro = true;
+	}
+}
